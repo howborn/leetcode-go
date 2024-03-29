2ef65778f2a819b0da8e1c631d6d7600abf33b5c
howborn <fanhaobai@gmail.com>
2024-03-29 17:42:39 +0800

    feat: add 69

---
+++ 二分查找/[69] x的平方根.go
@@ -0,0 +1,60 @@
+package main
+
+//给你一个非负整数 x ，计算并返回 x 的 算术平方根 。
+//
+// 由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
+//
+// 注意：不允许使用任何内置指数函数和算符，例如 pow(x, 0.5) 或者 x ** 0.5 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：x = 4
+//输出：2
+//
+//
+// 示例 2：
+//
+//
+//输入：x = 8
+//输出：2
+//解释：8 的算术平方根是 2.82842..., 由于返回类型是整数，小数部分将被舍去。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 0 <= x <= 2³¹ - 1
+//
+//
+// Related Topics 数学 二分查找 👍 1529 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func mySqrt(x int) int {
+	if x < 1 {
+		return x
+	}
+
+	left, right := 1, x
+	var mid, pow int
+	for left <= right {
+		mid = (right + left) / 2
+		pow = mid * mid
+		if pow < x {
+			left = mid + 1
+		} else if pow > x {
+			right = mid - 1
+		} else {
+			return mid
+		}
+	}
+
+	// 结束时right < left
+	return right
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
